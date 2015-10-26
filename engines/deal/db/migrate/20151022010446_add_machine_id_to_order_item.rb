7547dea73ec51e42453ac10bbb23785358d86526
Elton Silva <elton.chrls@gmail.com>
2015-10-26 16:15:16 -0200

    ok

--- engines/deal/db/migrate/20151022010446_add_machine_id_to_order_item.rb
+++
@@ -1,8 +0,0 @@
-class AddMachineIdToOrderItem < ActiveRecord::Migration
-  def change
-    add_column :deal_order_items, :machine_id, :string
-    add_column :deal_order_items, :machine_version, :string
-    add_column :deal_order_items, :machine_serial, :string
-    add_column :deal_order_items, :contract_value, :float
-  end
-end
