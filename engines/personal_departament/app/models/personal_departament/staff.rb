ee6c75279170cc80215edb4db43ab7bf10472540
Elton Silva <elton.chrls@gmail.com>
2015-11-16 15:05:24 -0200

    correções de campos

--- engines/personal_departament/app/models/personal_departament/staff.rb
+++ engines/personal_departament/app/models/personal_departament/staff.rb
@@ -9,12 +9,13 @@ module PersonalDepartament
     enum situation: [:experiência, :contratado, :demitido, :justa_causa, :afastado]
     enum civil_state: [:solteiro, :uniao_estavel]
     enum gender: [:masculino, :feminino]
-    
+
     validates :name, :code, :rg, :cpf, :rg_org, :rg_date, :pis, :ctps, :ctps_serie,
               :pis, :voter_registration, :voter_zone, :voter_session, :born,
               :bank, :bank_agency, :bank_account, :address, :burgh, :city, :state, 
               :cep, :telephone, :name_father, :name_mother, :gender, :naturality, 
               :civil_state, :education, :salary, :job, :sector, :contract, :height,
-              :weight, :blood, :badge_name, :situation, presence: true
+              :weight, :blood, :badge_name, :situation, :situation, :civil_state,
+              :salary, :work_start, :work_end, presence: true
   end
 end
