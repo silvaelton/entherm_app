fe2f695f6f2014ccb29b51414b7031af818fc7bb
Elton Silva <elton.chrls@gmail.com>
2016-04-19 15:36:45 -0300

    ok

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,4 @@ Rails.application.routes.draw do
 
   mount Deal::Engine        => '/compras',      as: 'deal',         module: 'deal'
   mount Access::Engine      => '/acesso',       as: 'access',       module: 'access'
-  mount Information::Engine => '/info',         as: 'information',  module: 'information'
-  mount Report::Engine      => '/relatorios',   as: 'report',       module: 'report'
-
-
-  mount PersonalDepartament::Engine  => '/departamento_pessoal',  as: 'personal_departament',   module: 'personal_departament'
 end
