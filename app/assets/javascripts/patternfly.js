667dfb7b838e713688c995a307ee38efd13df543
Elton Silva <elton.chrls@gmail.com>
2016-04-19 15:14:03 -0300

    nova interface

---
+++ app/assets/javascripts/patternfly.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+//= require patternfly/javascripts/bootstrap.min
+//= require patternfly/javascripts/jquery.mask.min
+//= require patternfly/javascripts/jquery.maskmoney.min
+//= require patternfly/javascripts/custom
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,5 +8,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( print.css )
-Rails.application.config.assets.precompile += %w( session.css session.js )
+Rails.application.config.assets.precompile += %w( print.css patternfly.css )
+Rails.application.config.assets.precompile += %w( session.css session.js patternfly.js)
